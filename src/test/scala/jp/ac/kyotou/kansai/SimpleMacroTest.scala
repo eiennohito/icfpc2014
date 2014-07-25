e1b4e5bac1a2fea249d639f680bc8d820e887773
Arseny Tolmachev <eiennohito@gmail.com>
2014-07-26 00:24:12 +0900

    committed a test using a scalatest framework

---
+++ src/test/scala/jp/ac/kyotou/kansai/SimpleMacroTest.scala
@@ -0,0 +1,28 @@
+package jp.ac.kyotou.kansai
+
+import org.scalatest.{Matchers, FreeSpec}
+
+/**
+ * @author eiennohito
+ * @since 2014-07-26
+ */
+class SimpleMacroTest extends FreeSpec with Matchers {
+  "macro ast transformer" - {
+    "works with a simple macro" in {
+      MacroTest.codeTest(20) should equal (60)
+    }
+
+    "transforms ast to a list" in {
+      val ast = MacroTest.code {
+        val i = 20
+      }
+
+      ast should have length (1)
+      ast.head should have (
+        'name ("i"),
+        'value (20)
+      )
+    }
+  }
+
+}

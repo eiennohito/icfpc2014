aaf12bb0be15677ca6f16c3520e94163eacb7ce1
Shotaro Kohama <khmshtr28@gmail.com>
2014-07-26 20:37:57 +0900

    add ai.scala

--- src/main/scala/jp/ac/kyotou/kansai/AI.scala
+++ src/main/scala/jp/ac/kyotou/kansai/AI.scala
@@ -17,12 +17,34 @@ class AI extends Support {
 	return mod(dir + 1, 4)
   }
 
-  def step(state : (Int, Int)) : (Int, Int) = {
-    return (1, 1)
+  def step(state : (MyList[Int], Int)) : (MyList[Int], Int) = {
+    var state_ = state._1
+    var dir = state_.cdr.cdr.car
+    var cur = state_.car
+    var step = state_.cdr.car
+    var ndir = next_dir(cur, step, dir)
+    var ncur = cur;
+    var nstep = step
+    if (cur == step) {
+      ncur = 0
+      nstep = nstep + 1
+    } else {
+      ncur = cur + 1
+    }
+
+    return (MyCons(ncur, MyCons(nstep, MyCons(ndir, MyNil))), ndir)
   }
 
-  def entryPoint(world: Int, undoc: Int): (Int, ((Int, Int)) => (Int, Int)) = {
-    return (42, step)
+  def next_dir(cur:Int, step:Int, dir:Int): Int = {
+    if (cur == step) {
+      return turnRight(dir)
+    } 
+    return dir
+  }
+
+  def entryPoint(world: Int, undoc: Int): (MyList[Int], ((MyList[Int], Int)) => (MyList[Int], Int)) = {
+    var init_state = MyCons(0, MyCons(1, MyCons(0, MyNil)))
+    return (init_state, step)
   }
 }
 
@@ -33,8 +55,12 @@ object AI extends AstCleanup {
     var gen = NameGen()
 	var codeList = CodeGen.emitStructure(cleanAsts.get("entryPoint").get, NameGen())
        codeList ++= CodeGen.emitStructure(cleanAsts.get("step").get, NameGen())
-    println(codeList.map(CodeGen.show).mkString("", "\n", ""))
-    println("-----")
+       codeList ++= CodeGen.emitStructure(cleanAsts.get("turnRight").get, NameGen())
+       codeList ++= CodeGen.emitStructure(cleanAsts.get("mod").get, NameGen())
+       codeList ++= CodeGen.emitStructure(cleanAsts.get("next_dir").get, NameGen())
+       codeList ++= List(Label("terminate"))
+    // println(codeList.map(CodeGen.show).mkString("", "\n", ""))
+    // println("-----")
     println(CodeGen.dereferenceLabels(codeList).map(CodeGen.show).mkString("", "\n", ""))
   }
 }

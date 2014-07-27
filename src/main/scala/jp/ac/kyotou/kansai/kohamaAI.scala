4ae45811993d89852bbc9c205790f55d1ba1b773
Shotaro Kohama <khmshtr28@gmail.com>
2014-07-27 12:31:40 +0900

    Add kohamaAI.scala

---
+++ src/main/scala/jp/ac/kyotou/kansai/kohamaAI.scala
@@ -0,0 +1,66 @@
+package jp.ac.kyotou.kansai
+
+@gccCode
+class kohamaAI extends Support {
+  def mod(a: Int, b: Int): Int = {
+    return a - (a / b) * b
+  }
+
+  def max(a: Int, b:Int): Int = {
+    if (a > b) {
+      return a
+    }
+    return b
+  }
+
+  def random(a: Int): Int = {
+    return mod(a * 1664525 + 69081, 10000003)
+  }
+
+  def turnRight(dir : Int): Int = {
+	return mod(dir + 1, 4)
+  }
+
+  def step(state : (Int, Int, Int), world : (Int, (Int, (Int, Int)))) : (((Int, Int, Int)), Int) = {
+    var pos = tupleLast(tupleLast(world, 2), 2)
+    var nx = pos._1
+    var ny = tupleLast(pos, 2)
+
+    var px = state._1
+    var py = state._2
+    var seed = tupleLast(state, 3)
+
+    if (px == nx) {
+      if (py == ny) {
+        seed = random(seed)
+      }
+    }
+   var dir = mod(seed, 4)
+
+    return ((nx, ny, seed), dir)
+  }
+
+  def entryPoint(world: (Int, (Int, (Int, Int))) , undoc: Int): ((Int, Int, Int),  ((Int, Int, Int), (Int, (Int, (Int, Int)))) => ((Int, Int, Int), Int)) = {
+    var pos = tupleLast(tupleLast(world, 2), 2)
+    var x = pos._1
+    var y = tupleLast(pos, 2)
+    var seed = 8
+    return ((x, y, seed), step)
+  }
+}
+
+object kohamaAI extends AstCleanup {
+  val asts = ???
+
+  def main(args: Array[String]) {
+    var gen = NameGen()
+	var codeList = CodeGen.emitStructure(cleanAsts.get("entryPoint").get, NameGen())
+       codeList ++= CodeGen.emitStructure(cleanAsts.get("step").get, NameGen())
+       codeList ++= CodeGen.emitStructure(cleanAsts.get("mod").get, NameGen())
+       codeList ++= CodeGen.emitStructure(cleanAsts.get("random").get, NameGen())
+       codeList ++= List(Label("terminate"))
+    println(codeList.map(CodeGen.show).mkString("", "\n", ""))
+    println("-----")
+    println(CodeGen.dereferenceLabels(codeList).map(CodeGen.show).mkString("", "\n", ""))
+  }
+}

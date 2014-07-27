be814e26976a9ab395c31366cd81746cd6fa9db1
Kazunori Tamura <sunes232002@gmail.com>
2014-07-27 16:54:58 +0900

    modify lib.scala

--- lib.scala
+++ lib.scala
@@ -1,56 +1,60 @@
-def arraySize(lst : MyList[Int]) : Int = {
-  if (lst == MyNil) {
-    return 0
-  }
-  return 1 + arraySize(lst.cdr)
-}
+package jp.ac.kyotou.kansai
 
-def arrayInit(n : Int, value : Int) : MyList[Int] = {
-  if (n == 0) {
-    return MyNil
+class lib extends Support {
+  def arraySize(lst : MyList[Int]) : Int = {
+    if (lst == MyNil) {
+      return 0
+    }
+    return 1 + arraySize(lst.cdr)
   }
-  return MyCons(value, arrayInit(n - 1, value))
-}
 
-def arrayGet(lst : MyList[Int], index : Int) : Int = {
-  if (index == 0) {
-    return lst.car
+  def arrayInit(n : Int, value : Int) : MyList[Int] = {
+    if (n == 0) {
+      return MyNil
+    }
+    return MyCons(value, arrayInit(n - 1, value))
   }
-  return arrayGet(lst.cdr, index - 1)
-}
 
-def arraySet(lst : MyList[Int], index : Int, value : Int) : MyList[Int] = {
-  if (lst == MyNil) {
-    return MyNil
+  def arrayGet(lst : MyList[Int], index : Int) : Int = {
+    if (index == 0) {
+      return lst.car
+    }
+    return arrayGet(lst.cdr, index - 1)
   }
-  var tmp = lst.car
-  if (index == 0) {
-    tmp = value
-  }
-  return MyCons(tmp, arraySet(lst.cdr, index - 1, value))
-}
 
-def arrayInit2D(n : Int, m : Int, value : Int) : MyList[MyList[Int]] = {
-  if (n == 0) {
-    return MyNil
+  def arraySet(lst : MyList[Int], index : Int, value : Int) : MyList[Int] = {
+    if (lst == MyNil) {
+      return MyNil
+    }
+    var tmp = lst.car
+    if (index == 0) {
+      tmp = value
+    }
+    return MyCons(tmp, arraySet(lst.cdr, index - 1, value))
   }
-  return MyCons(arrayInit(m, value), arrayInit2D(n - 1, m, value))
-}
 
-def arrayGet2D(lst : MyList[MyList[Int]], y : Int, x : Int) : Int = {
-  if (y == 0) {
-    return arrayGet(lst.car, x)
+  def arrayInit2D(n : Int, m : Int, value : Int) : MyList[MyList[Int]] = {
+    if (n == 0) {
+      return MyNil
+    }
+    return MyCons(arrayInit(m, value), arrayInit2D(n - 1, m, value))
   }
-  return arrayGet2D(lst.cdr, y-1, x)
-}
 
-def arraySet2D(lst : MyList[MyList[Int]], y : Int, x : Int, value : Int) : MyList[MyList[Int]] = {
-  if (lst == MyNil) {
-    return MyNil
+  def arrayGet2D(lst : MyList[MyList[Int]], y : Int, x : Int) : Int = {
+    if (y == 0) {
+      return arrayGet(lst.car, x)
+    }
+    return arrayGet2D(lst.cdr, y-1, x)
   }
-  var tmp = lst.car
-  if (y == 0) {
-    tmp = arraySet(lst.car, x, value)
+
+  def arraySet2D(lst : MyList[MyList[Int]], y : Int, x : Int, value : Int) : MyList[MyList[Int]] = {
+    if (lst == MyNil) {
+      return MyNil
+    }
+    var tmp = lst.car
+    if (y == 0) {
+      tmp = arraySet(lst.car, x, value)
+    }
+    return MyCons(tmp, arraySet2D(lst.cdr, y - 1, x, value))
   }
-  return MyCons(tmp, arraySet2D(lst.cdr, y - 1, x, value))
 }

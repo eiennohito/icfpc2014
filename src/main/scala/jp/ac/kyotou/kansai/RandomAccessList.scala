7cf8269b5d92c2710ee97ebdb84dccada881a1d2
chiro <peanut2626@gmail.com>
2014-07-28 14:50:35 +0900

    Update RandomAccessList.

--- src/main/scala/jp/ac/kyotou/kansai/RandomAccessList.scala
+++ src/main/scala/jp/ac/kyotou/kansai/RandomAccessList.scala
@@ -8,135 +8,133 @@ class RandomAccessList extends Support {
    lookup_RAL, update_RAL, size_RAL: O(log n)
    */
 
-  case class Tree(x: Int, ch: MyList[Tree])
-  def leaf_RAL(v: Int) = Tree(v, MyNil)
-  def isLeaf_RAL(v: Tree) = v.ch == MyNil
-  def node_RAL(v: Int, l: Tree, r: Tree) = Tree(v, MyList(l, r))
-  def leftChild_RAL(t: Tree) = t.ch.car
-  def rightChild_RAL(t: Tree) = t.ch.cdr.car
+  case class Tree[T](w: Int, v: MyList[T], ch: MyList[Tree[T]])
+  def leaf_RAL[T](t: T) = Tree[T](1, MyList(t), MyNil)
+  def emptyLeaf_RAL[T]() = Tree[T](1, MyNil, MyNil)
+  def isLeaf_RAL[T](v: Tree[T]) = v.ch == MyNil
+  def node_RAL[T](v: T, l: Tree[T], r: Tree[T]) = {
+    Tree[T](sizeTree_RAL(l) + sizeTree_RAL(r), MyList(v), MyList(l, r))
+  }
+  def leftChild_RAL[T](t: Tree[T]) = t.ch.car
+  def rightChild_RAL[T](t: Tree[T]) = t.ch.cdr.car
 
-  case class Digit(d: Int, t: Tree)
+  case class Digit[T](d: Int, t: Tree[T])
   // type RList = MyList[Digit]
-  def isEmpty_RAL(l: MyList[Digit]) = l == MyNil
-  def zero_RAL(): Digit = Digit(0, leaf_RAL(-100))
+  def isEmpty_RAL[T](l: MyList[Digit[T]]) = l == MyNil
+  def zero_RAL[T](): Digit[T] = Digit[T](0, emptyLeaf_RAL[T]())
 
-  def sizeTree_RAL(t: Tree): Int = {
+  def sizeTree_RAL[T](t: Tree[T]): Int = {
     if (isLeaf_RAL(t)) {
       return 1
     } else {
-      return t.x
+      return t.w
     }
   }
-  def link_RAL(t1: Tree, t2: Tree): Tree = Tree(sizeTree_RAL(t1) + sizeTree_RAL(t2), MyList(t1, t2))
-  def consTree_RAL(t: Tree, l: MyList[Digit]): MyList[Digit] = {
-    if (isEmpty_RAL(l)) {
-      return MyList(Digit(1, t))
+  def link_RAL[T](t1: Tree[T], t2: Tree[T]): Tree[T] = {
+    Tree(sizeTree_RAL[T](t1) + sizeTree_RAL[T](t2), MyNil, MyList(t1, t2))
+  }
+  def consTree_RAL[T](t: Tree[T], l: MyList[Digit[T]]): MyList[Digit[T]] = {
+    if (isEmpty_RAL[T](l)) {
+      return MyList(Digit[T](1, t))
     } else {
       if (l.car.d == 0) {
-        return MyCons(Digit(1, t), l.cdr)
+        return MyCons(Digit[T](1, t), l.cdr)
       } else {
-        return MyCons(zero_RAL(), consTree_RAL(link_RAL(t, l.car.t), l.cdr))
+        return MyCons(zero_RAL[T](), consTree_RAL[T](link_RAL[T](t, l.car.t), l.cdr))
       }
     }
   }
 
-  def unconsTreeAux_RAL(t: MyList[Digit]): (Tree, MyList[Digit]) = {
+  def unconsTreeAux_RAL[T](t: MyList[Digit[T]]): (Tree[T], MyList[Digit[T]]) = {
     var ts = unconsTree_RAL(t.cdr)
-    return (leftChild_RAL(ts._1), MyCons(Digit(1, rightChild_RAL(ts._1)), ts._2))
+    return (leftChild_RAL(ts._1), MyCons(Digit[T](1, rightChild_RAL[T](ts._1)), ts._2))
   }
 
-  def unconsTree_RAL(t: MyList[Digit]): (Tree, MyList[Digit]) = {
-    var td: Digit = t.car
+  def unconsTree_RAL[T](t: MyList[Digit[T]]): (Tree[T], MyList[Digit[T]]) = {
+    var td: Digit[T] = t.car
     if (td.d == 1) {
       if (t.cdr == MyNil) {
         // [One t]
         return (td.t, MyNil)
       } else {
         // [One t, ... ]
-        return (td.t, MyCons(zero_RAL(), t.cdr))
+        return (td.t, MyCons(zero_RAL[T](), t.cdr))
       }
     } else {
       // [Zero, ... ]
-      return unconsTreeAux_RAL(t)
+      return unconsTreeAux_RAL[T](t)
     }
   }
 
-  def cons_RAL(x: Int, ts: MyList[Digit]) = consTree_RAL(leaf_RAL(x), ts)
-  def head_RAL(ts: MyList[Digit]) = unconsTree_RAL(ts)._1.x
-  def tail_RAL(ts: MyList[Digit]) = unconsTree_RAL(ts)._2
-  def size_RAL(ts: MyList[Digit]): Int = {
-    if (isEmpty_RAL(ts)) return 0
+  def cons_RAL[T](x: T, ts: MyList[Digit[T]]) = consTree_RAL[T](leaf_RAL[T](x), ts)
+  def head_RAL[T](ts: MyList[Digit[T]]) = unconsTree_RAL[T](ts)._1.v
+  def tail_RAL[T](ts: MyList[Digit[T]]) = unconsTree_RAL[T](ts)._2
+  def size_RAL[T](ts: MyList[Digit[T]]): Int = {
+    if (isEmpty_RAL[T](ts)) return 0
     if (ts.car.d == 0) {
-      return size_RAL(ts.cdr)
+      return size_RAL[T](ts.cdr)
     } else {
-      return sizeTree_RAL(ts.car.t) + size_RAL(ts.cdr)
+      return sizeTree_RAL[T](ts.car.t) + size_RAL[T](ts.cdr)
     }
   }
 
-  def lookupTree_RAL(i: Int, t: Tree): Int = {
+  def lookupTree_RAL[T](i: Int, t: Tree[T]): T = {
     if (isLeaf_RAL(t)) {
-      if (i == 0) {
-        return t.x
-      } else {
-        // return -100000 instead of raise error!
-        return -100000
-      }
+      return t.v.car
     } else {
-      if (i < (t.x / 2)) {
-        return lookupTree_RAL(i, t.ch.car)
+      if (i < (t.w / 2)) {
+        return lookupTree_RAL[T](i, leftChild_RAL(t))
       } else {
-        return lookupTree_RAL(i - (t.x / 2), t.ch.cdr.car)
+        return lookupTree_RAL[T](i - (t.w / 2), rightChild_RAL(t))
       }
     }
   }
-  def updateTree_RAL(i: Int, v: Int, t: Tree): Tree = {
+  def updateTree_RAL[T](i: Int, v: T, t: Tree[T]): Tree[T] = {
     if (isLeaf_RAL(t)) {
       if (i == 0) {
         return leaf_RAL(v)
       } else {
-        return leaf_RAL(-1999999)
+        return emptyLeaf_RAL()
       }
     } else {
-      if (i < (t.x / 2)) {
-        return node_RAL(t.x, updateTree_RAL(i, v, leftChild_RAL(t)), rightChild_RAL(t))
+      if (i < (t.w / 2)) {
+        return Tree[T](t.w, MyNil, MyList(updateTree_RAL[T](i, v, leftChild_RAL[T](t)), rightChild_RAL[T](t)))
       } else {
-        return node_RAL(t.x, leftChild_RAL(t), updateTree_RAL(i - (t.x / 2), v, rightChild_RAL(t)))
-      }
-    }
-  }
-
-  def lookup_RAL(i: Int, ral: MyList[Digit]): Int = {
-    if (ral.car.d == 0) {
-      return lookup_RAL(i, ral.cdr)
-    } else {
-      if (i < sizeTree_RAL(ral.car.t)) {
-        return lookupTree_RAL(i, ral.car.t)
-      } else {
-        return lookup_RAL(i - sizeTree_RAL(ral.car.t), ral.cdr)
+        return Tree[T](t.w, MyNil, MyList(leftChild_RAL[T](t), updateTree_RAL[T](i - (t.w / 2), v, rightChild_RAL[T](t))))
       }
     }
   }
 
-  def update_RAL(i: Int, v: Int, ral: MyList[Digit]): MyList[Digit] = {
+  def lookup_RAL[T](i: Int, ral: MyList[Digit[T]]): T = {
     if (ral.car.d == 0) {
-      return MyCons(zero_RAL(), update_RAL(i, v, ral.cdr))
+      return lookup_RAL[T](i, ral.cdr)
     } else {
-      if (i < sizeTree_RAL(ral.car.t)) {
-        return MyCons(Digit(1, updateTree_RAL(i, v, ral.car.t)), ral.cdr)
+      if (i < sizeTree_RAL[T](ral.car.t)) {
+        return lookupTree_RAL[T](i, ral.car.t)
       } else {
-        return MyCons(Digit(1, ral.car.t), update_RAL(i - sizeTree_RAL(ral.car.t), v, ral.cdr))
+        return lookup_RAL[T](i - sizeTree_RAL[T](ral.car.t), ral.cdr)
+      }
+    }
+  }
+
+  def update_RAL[T](i: Int, v: T, ral: MyList[Digit[T]]): MyList[Digit[T]] = {
+    if (ral.car.d == 0) {
+      return MyCons(zero_RAL[T](), update_RAL[T](i, v, ral.cdr))
+    } else {
+      if (i < sizeTree_RAL[T](ral.car.t)) {
+        return MyCons(Digit[T](1, updateTree_RAL[T](i, v, ral.car.t)), ral.cdr)
+      } else {
+        return MyCons(Digit[T](1, ral.car.t), update_RAL[T](i - sizeTree_RAL[T](ral.car.t), v, ral.cdr))
       }
     }
   }
 
   def ent(): Int = {
-    var tree: Tree = leaf_RAL(1)
-    debug(tree)
 
-    debug(cons_RAL(1, MyNil))
-    debug(cons_RAL(2, cons_RAL(1, MyNil)))
+    debug(cons_RAL[Int](1, MyNil))
+    debug(cons_RAL[Int](2, cons_RAL(1, MyNil)))
 
-    var ral: MyList[Digit] = cons_RAL(1, cons_RAL(2, cons_RAL(3, cons_RAL(4, MyNil))))
+    var ral: MyList[Digit[Int]] = cons_RAL(1, cons_RAL(2, cons_RAL(3, cons_RAL(4, MyNil))))
     debug(head_RAL(ral))
     debug(head_RAL(tail_RAL(ral)))
     debug(head_RAL(tail_RAL(tail_RAL(ral))))
@@ -156,6 +154,16 @@ class RandomAccessList extends Support {
     debug(lookup_RAL(3, ral))
     debug(lookup_RAL(4, ral))
     debug(size_RAL(ral))
+
+    //
+    var ral2: MyList[Digit[(Int, Int)]] = cons_RAL((1, 1), cons_RAL((2, 2), cons_RAL((3, 3), MyNil)))
+    debug(lookup_RAL(0, ral2))
+    debug(lookup_RAL(0, ral2)._1)
+    debug(lookup_RAL(0, ral2)._2)
+    debug(lookup_RAL(1, ral2))
+    debug(lookup_RAL(2, ral2))
+    ral2 = update_RAL(1, (5, 5), ral2)
+    debug(lookup_RAL(1, ral2))
     return 0
   }
 }
